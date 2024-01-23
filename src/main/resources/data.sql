e3deacfa2b027100d9d4866e2a5efeeca3e48e5e
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-01-23 14:00:02 +0100

    bbdd

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Voluntarios (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    din VARCHAR(20),
+    direccion VARCHAR(100),
+    email VARCHAR(50),
+    contraseña VARCHAR(50),
+    imagen_perfil VARCHAR(255)
+);
