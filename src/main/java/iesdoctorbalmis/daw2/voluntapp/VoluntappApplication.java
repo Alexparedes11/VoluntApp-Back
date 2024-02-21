13050a4b3532abf1569e479210fc47c198562f11
noelbalmis <noel.leon@iesdoctorbalmis.com>
2024-02-21 20:58:08 +0100

    Añade noticias y fotos en Azure

--- src/main/java/iesdoctorbalmis/daw2/voluntapp/VoluntappApplication.java
+++ src/main/java/iesdoctorbalmis/daw2/voluntapp/VoluntappApplication.java
@@ -99,47 +99,80 @@ public class VoluntappApplication {
 			listaInstituciones.add(instituciones);
 
 			Instituciones instituciones2 = new Instituciones(null, "cruz@example.com",
-			"12345678B", "Cruz ORG", "616 616 616", "Manolito", "Cruz Roja", "123",
-			"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp", listaEventosInstituciones2);
+					"12345678B", "Cruz ORG", "616 616 616", "Manolito", "Cruz Roja", "123",
+					"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp", listaEventosInstituciones2);
 			institucionesService.guardar(instituciones2);
 			listaInstituciones.add(instituciones2);
 
 			Usuarios usuario = new Usuarios(null, "Manuel", "manuel@example.com",
 					"Rodriguez Martinez", "12345678A", "Calle Alfonso el sabio 1", "123",
-					"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp","https://voluntapp.blob.core.windows.net/images/banners/default.webp", "612 612 612", "Usuario", listaEventosUsuarios);
+					"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp",
+					"https://voluntapp.blob.core.windows.net/images/banners/default.webp", "612 612 612", "Usuario",
+					listaEventosUsuarios);
 			usuariosService.guardar(usuario);
 			listaUsuarios.add(usuario);
 
 			Usuarios usuario2 = new Usuarios(null, "Jose Miguel", "jose@example.com",
-					"Muñoz Vazquez", "21436587B", "Calle Teulada 10", "123", "https://voluntapp.blob.core.windows.net/images/perfiles/default.webp", 
+					"Muñoz Vazquez", "21436587B", "Calle Teulada 10", "123",
+					"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp",
 					"https://voluntapp.blob.core.windows.net/images/banners/default.webp",
 					"613 613 613", "Usuario", listaEventosUsuarios2);
 			usuariosService.guardar(usuario2);
 			listaUsuarios.add(usuario2);
 
 			Usuarios usuario3 = new Usuarios(null, "Administrador", "admin@example.com",
-					"Admin", "Inventado", "", "123", "https://voluntapp.blob.core.windows.net/images/perfiles/default.webp"
-					,"https://voluntapp.blob.core.windows.net/images/banners/default.webp", "", "Admin", listaEventosUsuarios3);
+					"Admin", "Inventado", "", "123",
+					"https://voluntapp.blob.core.windows.net/images/perfiles/default.webp",
+					"https://voluntapp.blob.core.windows.net/images/banners/default.webp", "", "Admin",
+					listaEventosUsuarios3);
 			usuariosService.guardar(usuario3);
 			listaUsuarios.add(usuario3);
-
 
 			Noticias noticias1 = Noticias.builder()
 					.titulo("Limpieza masiva en el bosque de Alcoy")
 					.contenido(
 							"¡Únete a nosotros para una limpieza masiva en el bosque de Alcoy! ¡Ayúdanos a mantener nuestro bosque limpio y seguro!")
-					.imagen("/aseets/images/bosque.jpg")
+					.imagen("https://voluntapp.blob.core.windows.net/images/noticias/new1.webp")
 					.fecha(Date.valueOf(LocalDateTime.now().toLocalDate()))
 					.autor("VoluntApp")
 					.build();
 			noticiasService.guardar(noticias1);
+			
+			Noticias noticias2 = Noticias.builder()
+					.titulo("Inauguración del nuevo centro comunitario en Valencia")
+					.contenido(
+							"¡Ven y únete a la celebración de la inauguración del nuevo centro comunitario en Valencia! Habrá música en vivo, comida deliciosa y actividades para toda la familia.")
+					.imagen("https://voluntapp.blob.core.windows.net/images/noticias/new2.webp")
+					.fecha(Date.valueOf(LocalDateTime.now().toLocalDate()))
+					.autor("Ayuntamiento de Valencia")
+					.build();
+			noticiasService.guardar(noticias2);
 
+			Noticias noticias3 = Noticias.builder()
+					.titulo("Conferencia sobre conservación marina en Alicante")
+					.contenido(
+							"No te pierdas nuestra conferencia sobre la importancia de la conservación marina en Alicante. Expertos en el campo compartirán sus conocimientos y experiencias.")
+					.imagen("https://voluntapp.blob.core.windows.net/images/noticias/new3.webp")
+					.fecha(Date.valueOf(LocalDateTime.now().toLocalDate()))
+					.autor("Asociación de Conservación Marina")
+					.build();
+			noticiasService.guardar(noticias3);
+
+			Noticias noticias4 = Noticias.builder()
+					.titulo("Torneo benéfico de fútbol en Castellón")
+					.contenido(
+							"Participa en nuestro torneo benéfico de fútbol en Castellón para recaudar fondos para niños necesitados. ¡Únete a nosotros para una jornada llena de diversión y solidaridad!")
+					.imagen("https://voluntapp.blob.core.windows.net/images/noticias/new4.webp")
+					.fecha(Date.valueOf(LocalDateTime.now().toLocalDate()))
+					.autor("Fundación Niños Felices")
+					.build();
+			noticiasService.guardar(noticias4);
 
 			Eventos eventos1 = Eventos.builder()
 					.fInicio(fechaInicioEvento1)
 					.fFin(fechaFinEvento1)
 					.titulo("Taller para mayores")
-					.imagen("/assets/images/mayores.jpg")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event1.webp")
 					.descripcion(
 							"¡Descubre nuevas experiencias y conexiones en nuestro evento de talleres para personas mayores!")
 					.ubicacion(ubicacion)
@@ -148,26 +181,25 @@ public class VoluntappApplication {
 					.creadoPorUsuarios(usuario2)
 					.maxVoluntarios(10)
 					.build();
-					
+
 			eventosService.guardar(eventos1);
 			usuario.addEventos(eventos1);
 			usuariosService.editar(usuario);
 			instituciones.addEventos(eventos1);
 			institucionesService.editar(instituciones);
 			evento = eventosService.buscarPorId(eventos1.getId());
-        	usuarios = usuariosService.buscarPorId(usuario2.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario2.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
-			
 
 			Eventos eventos2 = Eventos.builder()
 					.fInicio(fechaInicioEvento2)
 					.fFin(fechaFinEvento2)
-					.titulo("Recogida de basura")
-					.imagen("/assets/images/limpiarbosque.jpg")
-					.descripcion("Te invitamos a unirte a nosotros en un esfuerzo comunitario para recoger basura")
+					.titulo("Recogida de alimentos")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event2.webp")
+					.descripcion("¡Únete a nosotros en nuestra recogida de alimentos!")
 					.ubicacion(ubicacion2)
 					.estado("disponible")
 					.categorias(listaCategorias)
@@ -180,8 +212,8 @@ public class VoluntappApplication {
 			instituciones2.addEventos(eventos2);
 			institucionesService.editar(instituciones2);
 			evento = eventosService.buscarPorId(eventos2.getId());
-        	usuarios = usuariosService.buscarPorId(usuario.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
@@ -190,8 +222,8 @@ public class VoluntappApplication {
 					.fInicio(fechaInicioEvento3)
 					.fFin(fechaFinEvento3)
 					.titulo("Limpiar pellets de la playa")
-					.imagen("/assets/images/pellets.jpg")
-					.descripcion("¡Únete a nosotros en la playa Postiguet para un evento de recogida de pellets")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event3.webp")
+					.descripcion("¡Únete a nosotros en la playa Postiguet para un evento de recogida de pellets!")
 					.ubicacion(ubicacion3)
 					.estado("disponible")
 					.categorias(listaCategorias)
@@ -200,8 +232,8 @@ public class VoluntappApplication {
 					.build();
 			eventosService.guardar(eventos3);
 			evento = eventosService.buscarPorId(eventos3.getId());
-        	usuarios = usuariosService.buscarPorId(usuario2.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario2.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
@@ -209,9 +241,9 @@ public class VoluntappApplication {
 			Eventos eventos4 = Eventos.builder()
 					.fInicio(fechaInicioEvento4)
 					.fFin(fechaFinEvento4)
-					.titulo("Recogida de alimentos")
-					.imagen("/assets/images/recogidaalimentos.jpg")
-					.descripcion("¡Únete a nosotros en nuestra recogida de alimentos para apoyar")
+					.titulo("Recogida de basura")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event4.webp")
+					.descripcion("Te invitamos a unirte a nosotros en un esfuerzo comunitario para recoger basura")
 					.ubicacion(ubicacion4)
 					.estado("disponible")
 					.categorias(listaCategorias)
@@ -220,20 +252,20 @@ public class VoluntappApplication {
 					.build();
 			eventosService.guardar(eventos4);
 			evento = eventosService.buscarPorId(eventos4.getId());
-        	usuarios = usuariosService.buscarPorId(usuario2.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario2.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
 			evento = eventosService.buscarPorId(eventos4.getId());
-        	institucion = institucionesService.buscarPorId(instituciones.getId());
-        	if (evento.isPresent()) {
+			institucion = institucionesService.buscarPorId(instituciones.getId());
+			if (evento.isPresent()) {
 				institucion.get().addEventos(evento.get());
 				institucionesService.editar(institucion.get());
 			}
 			evento = eventosService.buscarPorId(eventos4.getId());
-        	institucion = institucionesService.buscarPorId(instituciones2.getId());
-        	if (evento.isPresent()) {
+			institucion = institucionesService.buscarPorId(instituciones2.getId());
+			if (evento.isPresent()) {
 				institucion.get().addEventos(evento.get());
 				institucionesService.editar(institucion.get());
 			}
@@ -242,8 +274,8 @@ public class VoluntappApplication {
 					.fInicio(fechaInicioEvento5)
 					.fFin(fechaFinEvento5)
 					.titulo("Sonrisas para los pequeños")
-					.imagen("/assets/images/sonrisas.jpg")
-					.descripcion("¡Ven y únete a nosotros para un día lleno de risas y diversión")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event5.webp")
+					.descripcion("¡Ven y únete a nosotros para un día lleno de risas y diversión!")
 					.ubicacion(ubicacion)
 					.estado("finalizado")
 					.categorias(listaCategorias)
@@ -252,8 +284,8 @@ public class VoluntappApplication {
 					.build();
 			eventosService.guardar(eventos5);
 			evento = eventosService.buscarPorId(eventos5.getId());
-        	usuarios = usuariosService.buscarPorId(usuario.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
@@ -262,7 +294,7 @@ public class VoluntappApplication {
 					.fInicio(fechaInicioEvento6)
 					.fFin(fechaFinEvento6)
 					.titulo("Ayuda para limpiar las inundaciones")
-					.imagen("/assets/images/inundacion.jpg")
+					.imagen("https://voluntapp.blob.core.windows.net/images/eventos/event6.webp")
 					.descripcion(
 							"Únete a nosotros para un evento comunitario de apoyo y solidaridad frente a las inundaciones")
 					.ubicacion(ubicacion)
@@ -273,8 +305,8 @@ public class VoluntappApplication {
 					.build();
 			eventosService.guardar(eventos6);
 			evento = eventosService.buscarPorId(eventos6.getId());
-        	usuarios = usuariosService.buscarPorId(usuario2.getId());
-        	if (evento.isPresent()) {
+			usuarios = usuariosService.buscarPorId(usuario2.getId());
+			if (evento.isPresent()) {
 				usuarios.get().addEventos(evento.get());
 				usuariosService.editar(usuarios.get());
 			}
